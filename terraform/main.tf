e79cfe60a5052b7f6dc813b456acf76353513a8a
Morteza <44939554+Mozart4242@users.noreply.github.com>
2022-04-21 21:50:00 +0430

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = "2.10.0"
     }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.7.0"
+    }
   }
 }
 
